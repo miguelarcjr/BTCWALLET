adb08446fd91cd43822200962830603982951399
Miguel Junior <miguelarcjunior@gmail.com>
2023-11-11 11:08:15 -0400

    first commit

---
+++ src/createWallet.js
@@ -0,0 +1,34 @@
+
+const bip32 = require('bip32');
+const bip39 = require('bip39');
+const bitcoin = require('bitcoinjs-lib');
+
+// definir a rede
+const network = bitcoin.networks.bitcoin;
+
+
+
+// criando o mnemonic para a seed (palavras de senha)
+let mnemonic = bip39.generateMnemonic();
+const seed = bip39.mnemonicToSeedSync(mnemonic)
+
+
+//criando a raiz da carteira HD
+let root = bip32.fromSeed(seed, network)
+
+// derivação de carteiras ID
+const path = `m/49'/1'/0'/0`;
+
+//criando uma conta - par pvt-pub keys
+let account = root.derivePath(path);
+let node = account.derive(0).derive(0);
+let btcAddress = bitcoin.payments.p2pkh({
+    pubkey: node.publicKey,
+    network: network,
+}).address;
+
+console.log("Carteira gerada");
+console.log("Endereço: ", btcAddress);
+console.log("Chave privada:", node.toWIF());
+console.log("Seed", mnemonic)
+
